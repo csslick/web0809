b9a210d361df473b3cc20a3f83299b240f9e0b8b
csslick <digitone@naver.com>
2018-09-26 22:41:44 +0900

    add content about web design

--- js/app.js
+++ js/app.js
@@ -14,6 +14,7 @@ $(function(){
         /*** add here... ***/
         { url: 'docs/2018-09-27a.html' }, // 의사소통능력
         { url: 'docs/2018-09-27.html' }, // 자기소개서
+        { url: 'docs/2018-09-26.html' }, // 
     ]
 
     docs.map(function(doc){
