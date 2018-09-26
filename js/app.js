ff7e5616cbfa1b65e445764e3e9f0a801f5ba568
csslick <digitone@naver.com>
2018-09-26 21:07:29 +0900

    ajax load

---
+++ js/app.js
@@ -0,0 +1,23 @@
+function insertDom(url){
+    $.ajax({
+        url: url,
+        dataType: 'html',
+        success: function(data){
+            $('#root').append(data);
+        }
+    }).fail(function(){throw 'load error!!'})
+}
+
+$(function(){
+    var docs = [
+        { url: 'templates/header.html' },
+        /*** add here... ***/
+        { url: 'docs/2018-09-27a.html' }, // 의사소통능력
+        { url: 'docs/2018-09-27.html' }, // 자기소개서
+    ]
+
+    docs.map(function(doc){
+        console.log(doc.url);
+        insertDom(doc.url)
+    })
+})
