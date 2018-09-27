0c0a1ceabcfa2347c34014f0cf944547658ea955
csslick <digitone@naver.com>
2018-09-27 22:27:08 +0900

    post datetime update2

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ function insertDom(url){
         success: function(data){
             $('#root').append(data);
 
+            // 문서 날짜 정보(파일명 참조)
             var time = url
                         .replace('docs/', '')
                         .replace('.html', '');
@@ -19,7 +20,7 @@ $(function(){
         /*** add here... ***/
         { url: 'docs/2018-09-27a.html' }, // 의사소통능력
         { url: 'docs/2018-09-27.html' }, // 자기소개서
-        { url: 'docs/2018-09-26.html' }, // 
+        { url: 'docs/2018-09-26.html' }, // 웹 디자인이란
     ]
 
     docs.map(function(doc){
