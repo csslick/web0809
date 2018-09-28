234947dc1fa350893ac9b51fcdae1488b5bd1710
csslick <digitone@naver.com>
2018-09-28 16:10:33 +0900

    111

--- js/app.js
+++ js/app.js
@@ -18,7 +18,7 @@ $(function(){
     var docs = [
         { url: 'templates/header.html' },
         /*** add here... ***/
-        { url: 'docs/2018-09-27a.html' }, // 의사소통능력
+        { url: 'docs/2018-09-27.html' }, // 의사소통능력
         { url: 'docs/2018-09-27.html' }, // 자기소개서
         { url: 'docs/2018-09-26.html' }, // 웹 디자인이란
     ]
