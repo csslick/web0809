b1100bf3cd634241be75398ee34adbd11bb832f5
csslick <digitone@naver.com>
2018-10-18 12:36:04 +0900

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ function insertDom(url){
     $.ajax({
         url: url,
         dataType: 'html',
-        // async: false,
+        async: false,
         success: function(data){
             if(data != null){
                 $('#root').append(data);
@@ -21,6 +21,16 @@ $(function(){
     var docs = [
         { url: 'templates/header.html' },
         /*** add here... ***/
+        // { url: 'docs/2018-10-15a.html' }, // 포스터
+        { url: 'docs/2018-10-18.html' }, // 사무용 판매 쇼핑몰 분석
+        { url: 'docs/2018-10-16.html' }, // 폰트 활용 타이틀
+        { url: 'docs/2018-10-15b.html' }, // 적목현상 제거
+        { url: 'docs/2018-10-15.html' }, // 페인트 활용 인물 일러스트
+        { url: 'docs/2018-10-12-04.html' }, // 물감브러시 활용 프사
+        { url: 'docs/2018-10-12-02.html' }, // 브러시
+        { url: 'docs/2018-10-12-03.html' }, // 이미지 구도, 수평 기울기
+        { url: 'docs/2018-10-12.html' }, // 메인메뉴 목차 구성하기
+        { url: 'docs/2018-10-11-03-47.html' }, // 이미지 분할
         { url: 'docs/2018-10-11.html' }, // 웹사이트 분석 사례2
         { url: 'docs/2018-10-10_04-11.html' }, // 나비 자르기
         { url: 'docs/2018-10-10.html' }, // 포토샵 서울타워 광고 페이지
